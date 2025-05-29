50309a0449568077463480fc8d6f7bd0224c99e2
wizard.wu <wuyuanyuan2399@dingtalk.com>
2025-05-29 14:54:37 +0800

    support stream return

--- LangChain4j/src/main/java/com/wizard/langchain4j/controller/ChatModelController.java
+++ LangChain4j/src/main/java/com/wizard/langchain4j/controller/ChatModelController.java
@@ -2,9 +2,12 @@ package com.wizard.langchain4j.controller;
 
 import dev.langchain4j.model.chat.ChatModel;
 import dev.langchain4j.model.chat.StreamingChatModel;
+import dev.langchain4j.model.chat.response.ChatResponse;
+import dev.langchain4j.model.chat.response.StreamingChatResponseHandler;
 import dev.langchain4j.model.ollama.OllamaChatModel;
 import dev.langchain4j.model.ollama.OllamaStreamingChatModel;
 import dev.langchain4j.model.openai.internal.chat.UserMessage;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,6 +16,7 @@ import reactor.core.publisher.Flux;
 
 @RestController
 @RequestMapping("/chat")
+@Slf4j
 public class ChatModelController {
 
 
@@ -22,11 +26,29 @@ public class ChatModelController {
         final String resultString = getChatModel().chat(inputValue);
         return resultString;
     }
-    @RequestMapping("/ollama/stream")
+    @RequestMapping(value = "/ollama/stream", produces = "text/stream;charset=utf-8")
     public Flux<String> streamOllamaChatModel(@RequestParam("message") String inputValue) {
         return Flux.create(sink -> {
+            getStreamChatModel().chat(inputValue, new StreamingChatResponseHandler() {
+                @Override
+                public void onPartialResponse(String partialResponse) {
+                    sink.next(partialResponse);
+                }
 
+                @Override
+                public void onCompleteResponse(ChatResponse completeResponse) {
+                    sink.complete();
+                    log.info("complete response");
+                }
+
+                @Override
+                public void onError(Throwable error) {
+                    sink.error(error);
+                    log.error("error: ", error);
+                }
+            });
         });
+
     }
 
     private ChatModel getChatModel() {
