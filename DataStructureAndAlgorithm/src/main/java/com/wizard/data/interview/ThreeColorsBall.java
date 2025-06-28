73b1c5b9f83663504202fd81177716760dae42b7
wizard.wu <wuyuanyuan2399@dingtalk.com>
2025-06-28 21:41:10 +0800

    add the leetcode

---
+++ DataStructureAndAlgorithm/src/main/java/com/wizard/data/interview/ThreeColorsBall.java
@@ -0,0 +1,36 @@
+package com.wizard.data.leetcode;
+
+import java.util.*;
+
+public class Test {
+    public static void main(String[] args) {
+        int[] array = {2, 0, 2, 1, 1, 0};
+        threeColorSort(array);
+        for(int value : array) {
+            System.out.print(value + " ");
+        }
+    }
+    private static void threeColorSort(int[] array){
+        int leftIndex = 0;
+        int rightIndex = array.length - 1;
+
+        int currentIndex = 0;
+        while(currentIndex < rightIndex){
+            if(array[currentIndex] == 0){
+                swap(array, leftIndex, currentIndex);
+                leftIndex++;
+                currentIndex++;
+            }else if(array[currentIndex] == 2){
+                swap(array, rightIndex, currentIndex);
+                rightIndex--;
+            }else {
+                currentIndex++;;
+            }
+        }
+    }
+    private static void swap(int[] array, int i, int j){
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
